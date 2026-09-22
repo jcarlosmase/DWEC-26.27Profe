1c514e88b36532c115ed41dc27070c715aabb888
Juan C. Jiménez <jcarlosjmase@gmail.com>
2026-09-22 09:12:34 +0200

    Clase 22/09/2026

--- 01.helloworld/codigo.js
+++ 01.helloworld/codigo.js
@@ -1,2 +1,30 @@
 //Código javascript
-    console.log("hello CLASS!!");
+//let myVar = "my value";
+
+
+var fSuma = function (a,b){
+    return a+b;
+}
+
+console.log(fSuma(2,3));
+
+console.log(numero);
+var numero= 5;
+
+const PI = 3.14;
+console.log(PI)
+//PI = 0.14;
+
+//OBJETOS EN JS
+var obj1 = {marca: "seat", modelo: "ibiza", color: "blanco"};
+console.log(obj1.marca); //seat
+obj1.marca = "ford";
+console.log(obj1.marca); //ford
+console.log(obj1);
+
+//conversiones de tipos
+var x = 1;
+console.log(x+1); //2
+x="1";
+console.log(x+1); //11
+console.log(parseInt(x)+1); //2
